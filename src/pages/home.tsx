ef0b2c276cbdbc8b3e0bff772b4a27d6662357eb
zpskek <wjdghdwns0@gmail.com>
2021-09-16 17:46:41 +0900

    Add shopping-list count to right-nav-bar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,26 @@
 import Categories from '@components/Categories';
+import useAuth from '@hooks/useAuth';
+import { getShoppingList } from '@store';
 import { Link, Navbar, NavLeft, NavRight, NavTitle, Page } from 'framework7-react';
 import React from 'react';
 
-const HomePage = () => (
-  <Page name="home">
-    <Navbar>
-      <NavLeft>
-        <Link icon="las la-bars" panelOpen="left" />
-      </NavLeft>
-      <NavTitle>Houpang</NavTitle>
-      <NavRight>
-        <Link href="/shopping-list" iconF7="cart" iconBadge={3} badgeColor="red" />
-      </NavRight>
-    </Navbar>
-    <Categories />
-  </Page>
-);
+const HomePage = () => {
+  const { currentUser } = useAuth();
+  const shoppingList = getShoppingList(currentUser.id);
+
+  return (
+    <Page name="home">
+      <Navbar>
+        <NavLeft>
+          <Link icon="las la-bars" panelOpen="left" />
+        </NavLeft>
+        <NavTitle>Houpang</NavTitle>
+        <NavRight>
+          <Link href="/shopping-list" iconF7="cart" iconBadge={shoppingList.length} badgeColor="red" />
+        </NavRight>
+      </Navbar>
+      <Categories />
+    </Page>
+  );
+};
 export default React.memo(HomePage);
