afa76f9af11afe283485ceeca5594e2feda6feda
hongjun95 <hongjunjeong95@gmail.com>
2021-09-25 11:39:10 +0900

    Amend tablink on views.tsx

--- src/components/Views.tsx
+++ src/components/Views.tsx
@@ -13,8 +13,9 @@ import { findLikeList } from '@api';
 import { useSetRecoilState } from 'recoil';
 import { likeListAtom, shoppingListAtom } from '@atoms';
 import { UserRole } from '@interfaces/user.interface';
+import { PageRouteProps } from '@constants';
 
-const F7Views = () => {
+const F7Views = ({ f7route }: PageRouteProps) => {
   const { currentUser, isAuthenticated, authenticateUser, unAuthenticateUser } = useAuth();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const setLikeList = useSetRecoilState<Like>(likeListAtom);
@@ -50,18 +51,43 @@ const F7Views = () => {
 
   const loggedInViews = () => (
     <Views tabs className="safe-areas">
-      <Toolbar tabbar labels bottom>
-        <Link tabLink="#view-home" tabLinkActive icon="las la-home" text="홈" />
+      <Toolbar tabbar labels bottom className="">
+        {/* <Link tabLink="#view-home" tabLinkActive icon="las la-home" text="홈" />
         <Link tabLink="#view-items" icon="las la-gift" text="쇼핑" />
         <Link tabLink="#view-users" icon="las la-address-book" text="사용자" />
         <Link tabLink="#view-contacts" icon="las la-edit" text="문의하기" />
-        <Link tabLink="#view-mypage" icon="las la-user" text="마이페이지" />
+        <Link tabLink="#view-mypage" icon="las la-user" text="마이페이지" /> */}
+        <Link className="w-1/6 flex flex-col items-center" href="/" tabLinkActive icon="las la-home" text="홈" />
+        <Link className="w-1/6 flex flex-col items-center" href="/items?is_main=true/" icon="las la-gift" text="쇼핑" />
+        <Link
+          className="w-1/6 flex flex-col items-center"
+          href="/users?is_main=true"
+          icon="las la-address-book"
+          text="사용자"
+        />
+        <Link
+          className="w-1/6 flex flex-col items-center"
+          href="/contacts?is_main=true"
+          icon="las la-edit"
+          text="문의하기"
+        />
+        <Link
+          className="w-1/6 flex flex-col items-center"
+          href="/mypage?is_main=true"
+          icon="las la-user"
+          text="마이페이지"
+        />
       </Toolbar>
-      <View id="view-home" stackPages main tab tabActive url="/" iosDynamicNavbar={false} />
+      <View id="view-home" stackPages main iosDynamicNavbar={false} />
+      <View id="view-items" stackPages name="items" />
+      <View id="view-users" stackPages name="users" />
+      <View id="view-contacts" stackPages name="contacts" />
+      <View id="view-mypage" stackPages name="mypage" />
+      {/* <View id="view-home" stackPages main tab tabActive url="/" iosDynamicNavbar={false} />
       <View id="view-items" stackPages name="items" tab url="/items?is_main=true/" />
       <View id="view-users" stackPages name="users" tab url="/users?is_main=true" />
       <View id="view-contacts" stackPages name="contacts" tab url="/contacts?is_main=true" />
-      <View id="view-mypage" stackPages name="mypage" tab url="/mypage?is_main=true" />
+      <View id="view-mypage" stackPages name="mypage" tab url="/mypage?is_main=true" /> */}
     </Views>
   );
 
