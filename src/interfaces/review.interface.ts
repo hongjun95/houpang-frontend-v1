56d8bbcbe174ce79b3685beadcdd2e636510a570
hongjun95 <hongjunjeong95@gmail.com>
2021-09-28 09:46:43 +0900

    Add bone of review sector to product-detail page

--- src/interfaces/review.interface.ts
+++ src/interfaces/review.interface.ts
@@ -1,9 +1,11 @@
-import { CoreEntity } from "./core.interface";
+import { CoreEntity } from './core.interface';
 import { Product } from './product.interface';
 import { User } from './user.interface';
 
 export interface Review extends CoreEntity {
-  content: string;
   commenter: User;
   product: Product;
+  content: string;
+  grade: number;
+  images: string[];
 }
