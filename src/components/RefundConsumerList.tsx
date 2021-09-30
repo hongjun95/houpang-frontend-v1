2c9efd6b7aa8cd5b671f9e624be233f30ba90461
hongjun95 <hongjunjeong95@gmail.com>
2021-09-30 15:42:01 +0900

    Amend RefundConsumerList component to use useInfiniteQuery

--- src/components/RefundConsumerList.tsx
+++ src/components/RefundConsumerList.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useEffect } from 'react';
+import { useInfiniteQuery } from 'react-query';
 import { Router } from 'framework7/types';
+import { useInView } from 'react-intersection-observer';
 
 import { getRefundsFromConsumerAPI } from '@api';
 import { refundsFromConsumer } from '@reactQuery/query-keys';
 import { User } from '@interfaces/user.interface';
 import RefundItem from './RefundItem';
+import { GetRefundsFromConsumerOutput, Refund } from '@interfaces/refund.interface';
+import LandingPage from '@pages/landing';
 
 interface RefundConsumerListProps {
   currentUser: User;
@@ -13,20 +16,60 @@ interface RefundConsumerListProps {
 }
 
 const RefundConsumerList: React.FC<RefundConsumerListProps> = ({ currentUser }) => {
-  const { data, status } = useQuery(refundsFromConsumer.list({ consumerId: currentUser.id }), () =>
-    getRefundsFromConsumerAPI({ consumerId: currentUser.id }),
+  const { ref, inView, entry } = useInView({
+    threshold: 0,
+  });
+
+  const {
+    fetchNextPage, //
+    hasNextPage,
+    isFetchingNextPage,
+    isFetching,
+    data,
+    error,
+    status,
+  } = useInfiniteQuery<GetRefundsFromConsumerOutput, Error>(
+    refundsFromConsumer.list({ consumerId: currentUser.id, page: 1 }),
+    ({ pageParam }) => getRefundsFromConsumerAPI({ consumerId: currentUser.id, page: pageParam }),
+    {
+      getNextPageParam: (lastPage) => {
+        const hasNextPage = lastPage.hasNextPage;
+        return hasNextPage ? lastPage.nextPage : false;
+      },
+    },
   );
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching && entry.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetching]);
 
   return (
     <>
-      {status === 'success' && data?.refundItems.length === 0 ? (
+      {status === 'loading' ? (
+        <LandingPage />
+      ) : status === 'error' ? (
+        <span>Error : {error.message}</span>
+      ) : data?.pages[0].refundItems.length === 0 ? (
         <div className="flex items-center justify-center min-h-full">
           <span className="text-3xl font-bold text-gray-500">취소&#183;반품&#183;교환 목록이 비었습니다.</span>
         </div>
       ) : (
-        data?.refundItems.map((refundItem) => (
-          <RefundItem key={refundItem.id} userId={currentUser.id} refundItem={refundItem} isProviderList={false}/>
-        ))
+        <div>
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.refundItems.map((refundItem: Refund) => (
+                <RefundItem key={refundItem.id} userId={currentUser.id} refundItem={refundItem} isProviderList={true} />
+              ))}
+            </React.Fragment>
+          ))}
+          <div className="flex justify-center font-bold mt-4">
+            <div ref={hasNextPage && !isFetching ? ref : null}>
+              {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load Newer' : 'Nothing more to load'}
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
